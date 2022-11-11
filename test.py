e5960e17ad13aa54dca3e451fb1d1bf6b9ee54c1
Чо скужин? <79135478+SkuJin@users.noreply.github.com>
2022-11-11 11:57:21 +0300

    Hs

--- test.py
+++ test.py
@@ -4,8 +4,8 @@ from userbot.events import register
 
 @register(outgoing=True, pattern='^.bio ?(.*)')
 async def fakeload(e):
-	await.edit.e("'Bio
+	await.edit.e('Bio
 13 y.o
 Discord: SkuJin#1770
 Telegram: @Amongasik1337, @SkuJin
-donate 🥝 https://qiwi.com/n/SkuJin'")
+donate 🥝 https://qiwi.com/n/SkuJin')
