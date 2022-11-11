f57c151ec5d23f73a5be574e63efed32264424b9
Чо скужин? <79135478+SkuJin@users.noreply.github.com>
2022-11-11 12:03:45 +0300

    Add files via upload

---
+++ 2test.py
@@ -0,0 +1,11 @@
+from asyncio import sleep
+from userbot.events import register
+
+
+@register(outgoing=True, pattern='^.bio ?(.*)')
+async def fakeload(e):
+	await.e.edit('Bio
+13 y.o
+Discord: SkuJin#1770
+Telegram: @Amongasik1337, @SkuJin
+donate 🥝 https://qiwi.com/n/SkuJin')
